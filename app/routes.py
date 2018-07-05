70dfafa8103c5e3111f89845882def815bb3ebb9
Damien Robinson <damien.robinson@xcommedia.com.au>
2018-07-05 16:00:00 +1000

    Added template to route

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,9 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    return 
-    '''
-    <html>
-        <head>
-            <title>Home Page - Microblog</title>
-        </head>
-        <body>
-            <h1>Hello, ''' + user['username'] + '''!</h1>
-        </body>
-    </html>
-    '''
+    user = {'username': 'Damien'}
+    return render_template('index.html', title='Home', user=user)
 
